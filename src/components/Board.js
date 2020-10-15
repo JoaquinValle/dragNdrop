54c663ef1143fd525a9a1bbeebe8dcb7fd229f23
Joaquin Valle <jv346@cornell.edu>
2020-10-14 20:28:24 -0500

    feature: initial board/card logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-const Board = () => {
+const Board = (props, { id }) => {
+  const drop = event => {
+    event.preventDefault();
+    const cardId = event.dataTransfer.getData("id");
+
+    const card = document.getElementById(cardId);
+    card.style.display = "block";
+
+    event.target.appendChild(card);
+  };
+
+  const dragOver = event => {
+    event.preventDefault();
+  };
+  
   return (
-  <>
-    
-  </>
+    <div id={id} onDrop={drop} onDragOver={dragOver}>
+      {props.children}
+    </div>
   );
 };
 
